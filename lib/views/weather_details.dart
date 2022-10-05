84c453eda51098faf810054f14a51b5585da5b6e
Rafal Malarowski <rmalarowski@gmail.com>
2022-10-05 18:28:16 +0200

    third_part

--- lib/views/weather_details.dart
+++ lib/views/weather_details.dart
@@ -13,7 +13,13 @@ class WeatherDetails extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(this.weatherInfo['cityName']),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              this.weatherInfo['cityName'],
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+            ),
             // Text('Sunday, 1 AM'),
             Column(
               children: [
@@ -29,8 +35,91 @@ class WeatherDetails extends StatelessWidget {
                 ),
                 const SizedBox(height: 100),
                 Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.grey,
+                  ),
                   margin: const EdgeInsets.all(10),
-                  color: Colors.grey[100],
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: const [
+                              Text(
+                                'Pressure',
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 16),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                '1014hPa',
+                                style:
+                                    TextStyle(fontSize: 22, color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Text(
+                                'Wind',
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 16),
+                              ),
+                              Text(
+                                '50 km/h',
+                                style:
+                                    TextStyle(fontSize: 22, color: Colors.grey),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: const [
+                              Text(
+                                'Pressure',
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 16),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                '1014hPa',
+                                style:
+                                    TextStyle(fontSize: 22, color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Text(
+                                'Wind',
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 16),
+                              ),
+                              Text(
+                                '50 km/h',
+                                style:
+                                    TextStyle(fontSize: 22, color: Colors.grey),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
 
                 // const Text('Heavy Rain'),
